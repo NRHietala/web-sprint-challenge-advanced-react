9947fd26b3527e8c535abc9015d90306b0d72662
Nathan Hietala <Nrhietala@gmail.com>
2021-01-15 11:54:38 -0600

    Finished Tests

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -3,8 +3,16 @@ import { render, screen } from "@testing-library/react";
 import userEvent from '@testing-library/user-event';
 
 import CheckoutForm from "./CheckoutForm";
+import { act } from "react-dom/test-utils";
 
-// Write up the two tests here and make sure they are testing what the title shows
+const testInput = {
+  firstName: "Nat",
+  lastName: "Hietala",
+  address: "123 Test St.",
+  city: "Silver Town",
+  state:"MN",
+  zip:'55555'
+}
 
 test("Sanity Test/Loads with no Errors", () => {
   render(<CheckoutForm />)
@@ -21,10 +29,33 @@ test("form header renders", () => {
   expect(formHeader).toBeInTheDocument();
 });
 
-test("form shows success message on submit with form details", () => {
+test("form shows success message on submit with form details", async () => {
   // Arrange 
-  
+  render(<CheckoutForm />)
+
+  const firstName = screen.getByLabelText(/first name/i);
+  const lastName = screen.getByLabelText(/last name/i);
+  const address = screen.getByLabelText(/address/i);
+  const city = screen.getByLabelText(/city/i);
+  const state = screen.getByLabelText(/state/i);
+  const zipInput = screen.getByLabelText(/zip/i);
+
+  const button = screen.queryByRole('button')
+
   // Act
-  
+   act( () => {
+    userEvent.type(firstName, testInput.firstName);
+    userEvent.type(lastName, testInput.lastName);
+    userEvent.type(address, testInput.address);
+    userEvent.type(city, testInput.city);
+    userEvent.type(state, testInput.state);
+    userEvent.type(zipInput, testInput.zip);
+  })
+
+  await act( async () => {
+    userEvent.click(button)
+  })
+
   // Assert
+  expect(/successMessage/i).toBeInTheDocument;
 });
